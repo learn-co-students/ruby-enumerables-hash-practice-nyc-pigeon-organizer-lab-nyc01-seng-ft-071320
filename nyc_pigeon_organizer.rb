b9844a5fb34e2618c50b01080afd600f4189c0a1
david <dkim187@binghamton.edu>
2020-06-18 18:22:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ new_hash = {}
+ data.each do |key, value|
+    value.each do |inner_key_1, inner_value_1|
+      inner_value_1.each do |index|
+        if !new_hash[index]
+          new_hash[index] = {}
+        end
+          if !new_hash[index][key]
+            new_hash[index][key] = []
+            #creates the hash inside the hash
+          end
+            new_hash[index][key] << inner_key_1.to_s
+            #creates what does in the variable
+      end
+    end
+  end
+  new_hash
 end
