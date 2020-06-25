e7dbc4655ac9dbd6856eb2a9384beebf2b57636e
David Kirsch <davidjkirsch@gmail.com>
2020-06-25 18:56:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+ data.each_with_object({}) do |(k,v), final_hash|
+   v.each do |key, value|
+     value.each do |name|
+       if(final_hash[name] == nil)
+         final_hash[name] = {}
+       end
+       if(final_hash[name][k] == nil)
+         final_hash[name][k] = []
+       end
+     final_hash[name][k] << key.to_s  
+     end
+   end
+   final_hash
+ end
 end
