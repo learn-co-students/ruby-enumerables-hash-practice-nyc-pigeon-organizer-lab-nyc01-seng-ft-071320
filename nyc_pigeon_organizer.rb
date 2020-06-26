84bf6892590561d035ea26727f77cb12056257c6
David Kirsch <davidjkirsch@gmail.com>
2020-06-26 18:13:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,19 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
- data.each_with_object({}) do |(k,v), final_hash|
-   v.each do |key, value|
-     value.each do |name|
-       if(final_hash[name] == nil)
-         final_hash[name] = {}
-       end
-       if(final_hash[name][k] == nil)
-         final_hash[name][k] = []
-       end
-     final_hash[name][k] << key.to_s  
-     end
-   end
-   final_hash
- end
+  hash = {}
+  data.each do |key, value|
+    value.each do |inner_key, inner_value|
+      inner_value.each do |name|
+        if(hash[name] == nil)
+          hash[name] = {}
+        end
+        if(hash[name][key] == nil)
+          hash[name][key] = []
+        end
+        hash[name][key] << inner_key.to_s 
+      end
+    end
+  end
+  hash
 end
