e36ad1bc8fc840ec035a63a3d54cb16d9dcce17f
Amit Deshpande <amitdesh2014@gmail.com>
2020-07-01 00:48:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_dict = {}
+  data.each do |key1, hash|
+    hash.each do |key2, value|
+      value.each do |names|
+        #binding.pry
+        if !new_dict.has_key?(names)
+          new_dict[names] = {}
+        end
+        if !new_dict[names].has_key?(key1)
+          new_dict[names][key1] = []
+        end
+        if !new_dict[names][key1].include?(key2)
+          new_dict[names][key1].push(key2.to_s)
+        end
+      end
+    end
+  end
+  puts new_dict
+  return new_dict
 end
