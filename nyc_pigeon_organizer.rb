4aa7fa856d1bdd8cfdff0eb0c11e82f7881e2314
Junko Tahara <junkotahara911@gmail.com>
2020-07-10 03:31:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+
+def attribute_of_the_pigeon(data, attribute, pigeon)
+  attribute_list = []
+  
+  data[attribute].each { |attribute_key, names|
+    
+    names.each { |name| 
+      if name == pigeon
+        attribute_list << attribute_key.to_s
+      end
+    }
+    
+  }
+  attribute_list
 end
+
+
+def attribute(data, pigeon)
+  attribute_lists = {}
+  
+  data.each_key { |attribute|
+    attribute_lists[attribute] = attribute_of_the_pigeon(data, attribute, pigeon)
+  }
+  
+  attribute_lists
+end
+
+def nyc_pigeon_organizer(data)
+  new_list = {}
+  
+  data[:gender].each_value { |names| 
+    names.each { |name|
+      new_list[name] = attribute(data, name)
+    }
+  }
+  
+  new_list
+end
+
+
+
+binding.pry
